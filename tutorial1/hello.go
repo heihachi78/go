80e485c552244152921a3170513dd637489971d4
itoth1 <istvan.toth2@eos-hungary.hu>
2024-12-04 09:37:12 +0000

    multiple greetings

--- tutorial1/hello.go
+++ tutorial1/hello.go
@@ -15,10 +15,13 @@ import (
 func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	message, err := greetings.Hello("Hachi")
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	for _, message := range messages {
+		fmt.Println(message)
+	}
 	fmt.Println(quote.Go())
 }
